8b82bb4433fcee9a4464f899d7e5ba0cf74ee9ac
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-10-19 15:04:56 +0100

    basic boilerplate

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  const year = new Date().getFullYear()
+  return ( <footer className={styles.footer}>
+    <p>Copyright {year} Harry Duncton</p>
+     
+</footer>  );
+}
+
+export default Footer;
