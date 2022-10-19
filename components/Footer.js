153a1d5620b3b9a9435e1fbb46b1367f6166bd2c
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-10-19 16:16:39 +0100

    styles undefined in Footer, failed build

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   const year = new Date().getFullYear()
-  return ( <footer className={styles.footer}>
+  return ( <footer>
     <p>Copyright {year} Harry Duncton</p>
      
 </footer>  );
