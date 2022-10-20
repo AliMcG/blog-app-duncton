9db2f74b305cb4920b919a5363157a001c9ec3be
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-10-20 21:26:15 +0100

    added basic pages for about and contact

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function Contact() {
+  return ( <>
+    <h3>Contact details will go here</h3>
+    <h4>
+        <Link href="/">Back to home</Link>
+      </h4>
+  </> );
+}
+
+export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 // import Image from 'next/image'
+import Link from 'next/link';
 import Footer from "../components/Footer";
 import styles from "../styles/Home.module.css";
 
@@ -10,20 +11,26 @@ export default function Home() {
         <title>Harry Duncton</title>
         <meta name="description" content="A blog to capture the roving mind" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet" /> 
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Harry Duncton</h1>
 
         <p className={styles.description}>
-          NavBAr goes here (Home, About, Contact)
+          NavBAr goes here (<Link href="/">Home</Link>, <Link href="/about">About</Link>, <Link href="/contact">Contact</Link>)
         </p>
 
         <div className={styles.grid}>
+          
           <div className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+          <Link href="/posts/first-post" >
+            <h2>Post Link &rarr;</h2>
+            </Link>
             <p>Find in-depth information about Next.js features and API.</p>
+            
           </div>
+          
 
           <h2>Examples &rarr;</h2>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
