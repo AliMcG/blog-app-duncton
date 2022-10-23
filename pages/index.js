72c9fb6705397397aa17dc25ba14ebc917deb44a
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-10-23 21:36:28 +0100

    CSS post .card now more inline with wireframe design

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,22 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div className={styles.grid}>
+        
           <div className={styles.card}>
             <Link href="/posts/first-post">
               <h2>Post 1st Link &rarr;</h2>
             </Link>
             <p>A blog about blogging</p>
+            <p>An image goes here</p>
           </div>
           <div className={styles.card}>
             <Link href="/posts/first-post">
-              <h2>Post 2nd Link &rarr;</h2>
+              <h2>Post 2nd Link &rarr; </h2>
             </Link>
             <p>A blog about a new drink.</p>
+            <p>An image goes here</p>
           </div>
-        </div>
+        
       </main>
     </div>
   );
