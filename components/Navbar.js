d29c619a043bfe0547bad450cecab04d41923d5c
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-11-13 21:03:06 +0000

    header title pic centered and correct size

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,46 @@
-import styles from "../styles/Navbar.module.css"
-import Link from "next/link"
-import Image from 'next/image'
-import titleImage from "../public/harry-duncton-header_trans_.png"
+import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import titleImage from "../public/harry-duncton-header_trans_.png";
 // import title from "/public/HarryDunctonheader_trans_.png"
 
 function Navbar() {
+  const headerStyle = {
+    alignSelf: "center",
+    inset: "1rem",
+    position: "absolute",
+    // // top: "50%",
+    // left: "50%",
+    // right: "50%",
+    // transform: "translate(-50%, -50%)",
+    // backgroundColor: 'red',
+  };
   return (
     <>
-      <header>
+      <header className={styles.header}>
         {/* <h1 className={styles.title}>Harry Duncton</h1> */}
-        <Image src={titleImage} alt="Harry Duncton" width="400" height="200"/>
+        <Image
+          src={titleImage}
+          style={headerStyle}
+          alt="Harry Duncton"
+          width="1000"
+          height="200"
+        />
       </header>
       <nav className={styles.navbar}>
-        <p><Link href="/"><a>Home,</a></Link>{" "}<Link href="/about"><a>About,</a></Link>{" "}
-        <Link href="/contact"><a>Contact,</a></Link>{" "}<Link href="/posts"><a>Archive,</a></Link>{" "}<Link href="/upload"><a>Upload</a></Link></p>
+        
+        <Link href="/">
+          <a>HOME</a>
+        </Link>{" "}
+        <Link href="/about">
+          <a>ABOUT</a>
+        </Link>{" "}
+        <Link href="/contact">
+          <a>CONTACT</a>
+        </Link>{" "}
+        <Link href="/upload">
+          <a>(upload)</a>
+        </Link>
       </nav>
     </>
   );
