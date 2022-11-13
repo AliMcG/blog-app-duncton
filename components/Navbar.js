aab1f7727d6a2a4c97edf45e496d671f71a4569b
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-11-13 10:33:40 +0000

    title header is up but not centered

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,15 @@
 import styles from "../styles/Navbar.module.css"
 import Link from "next/link"
+import Image from 'next/image'
+import titleImage from "../public/harry-duncton-header_trans_.png"
+// import title from "/public/HarryDunctonheader_trans_.png"
 
 function Navbar() {
   return (
     <>
       <header>
-        <h1 className={styles.title}>Harry Duncton</h1>
+        {/* <h1 className={styles.title}>Harry Duncton</h1> */}
+        <Image src={titleImage} alt="Harry Duncton" width="400" height="200"/>
       </header>
       <nav className={styles.navbar}>
         <p><Link href="/"><a>Home,</a></Link>{" "}<Link href="/about"><a>About,</a></Link>{" "}
