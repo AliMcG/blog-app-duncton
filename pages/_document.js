3249e2aeb887fb537c6b55c659d7b0a0dc1ca8b2
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-11-21 10:09:33 +0000

    removed amplify again

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
-// This loads custom HTML links - like custom fonts
+// This loads custom HTML links - like custom fonts and anything that would go in HTML head tag.
 
 export default function Document() {
   return (
     <Html>
       <Head>
-      {/* <title>Harry Duncton</title> */} 
+        {/* <title>Harry Duncton</title> */}
         <meta name="description" content="A blog to capture the roving mind" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@700&family=Montserrat&display=swap" rel="stylesheet"/> 
-        {/* <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet" />  */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@700&family=Montserrat&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
