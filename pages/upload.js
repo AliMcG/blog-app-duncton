363cb7fe9282e0e6de3a8a2e5e6e220aa5625a9f
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-11-29 09:44:49 +0000

    working on image upload

--- pages/upload.js
+++ pages/upload.js
@@ -2,43 +2,68 @@
 // import { withAuthenticator } from "@aws-amplify/ui-react";
 // import "@aws-amplify/ui-react/styles.css";
 // import awsExports from "../src/aws-exports";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import BlogForm from "../components/BlogForm";
+import Image from "next/image";
 import styles from "../styles/UploadPage.module.css";
 
 // import { API, graphqlOperation } from "aws-amplify";
 // import { createTodo } from "../src/graphql/mutations";
 // Amplify.configure(awsExports);
 
-
-
 const initialState = {
   title: "",
   description: "",
+  image: "",
 };
 
 // { signOut, user }
 function AddNewBlog() {
-  
-  const [blog, setBlog] = useState(initialState)
-  const [imageFile, setImageFile] = useState(null);
+  const [state, dispatch] = useReducer(
+    (state, action) => ({
+      ...state,
+      ...action,
+    }),
+    initialState
+  );
+  // const [blog, setBlog] = useState(initialState)
+  // const [imageFile, setImageFile] = useState("");
+
+  const toBase64 = (file) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  const reader = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result);
+      fileReader.readAsDataURL(file);
+    });
+  };
+  const readFile = (file) => {
+    reader(file).then((result) => console.log("From reader", result));
+  };
+  const readBase = (file) => {
+    toBase64(file).then((result) => console.log("This is Base64", result));
+  };
 
   async function handleSubmit(e) {
     e.preventDefault();
-    
-    console.log("This is handleSubmit", blog, imageFile)
-    // await API.graphql(graphqlOperation(createTodo, { input: blog }));
-    
 
-    setBlog(initialState)
-    
+    console.log("This is handleSubmit", state);
+    // await API.graphql(graphqlOperation(createTodo, { input: blog }));
+    // setBlog(initialState)
+    dispatch(initialState);
   }
 
   function handleChange(event) {
     // destructs the event.target
     const { name, value } = event.target;
 
-     // using prevValue of the state object, the spread operator to spread then add the new value.
+    // using prevValue of the state object, the spread operator to spread then add the new value.
     // [name] reads the name from the input tag
     setBlog((prevValue) => {
       return { ...prevValue, [name]: value };
@@ -47,40 +72,41 @@ function AddNewBlog() {
 
   return (
     <main className={styles.title}>
-        <h1 >Write a new blog entry</h1>
-        {/* <form onSubmit={handleSubmit}> */}
-          {/* <input
-            type="text"
-            name="title"
-            placeholder="Title..."
-            value={blog.title}
-            onChange={handleChange}
-          /> */}
+      <h1>Write a new blog entry</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title..."
+          value={state.title}
+          // onChange={handleChange}
+          onChange={(e) => dispatch({ title: e.target.value })}
+        />
+        <textarea
+          name="description"
+          placeholder="Content..."
+          value={state.description}
+          rows="4"
+          cols="50"
+          // onChange={handleChange}
+          onChange={(e) => dispatch({ description: e.target.value })}
+        />
+        <label>
+          Add Image:
+          <br></br>
+          <input
+            type="file"
+            name="image"
+            value={state.image}
+            // onChange={(e) => setImageFile(e.target.value)}
+            onChange={(e) => dispatch({ image: readBase(e.target.value) })}
+          />
+        </label>
 
-
-          {/* <textarea
-            name="description"
-            placeholder="Content..."
-            value={blog.description}
-            rows="4"
-            cols="50"
-            onChange={handleChange}
-          /> */}
-          
-          {/* <label>
-            Add Image:
-            <br></br>
-            <input
-              type="file"
-              name="image"
-              value={imageFile}
-              onChange={(e) => setImageFile(e.target.value)}
-            />
-          </label> */}
-          
-          {/* <input type="submit" value="Submit" /> */}
-        {/* </form> */}
-        <BlogForm />
+        <input type="submit" value="Submit" />
+      </form>
+      {/* <BlogForm /> */}
+      <Image src={upLoadedFile} alt="upload image" width="200" height="200" />
     </main>
   );
 }
