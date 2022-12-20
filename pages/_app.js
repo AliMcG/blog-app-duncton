ad612ca72cb2b82d956e88462b299c25bd230b53
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-12-20 21:30:35 +0000

    upgrade to next 13

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
 import "../styles/globals.css";
 import {Layout} from "../components/Layout";
-import { Montserrat } from '@next/font/google'
+// import { Montserrat } from '@next/font/google'
 
-const montserrat = Montserrat({ subsets: ['latin'] })
+// const montserrat = Montserrat({ subsets: ['latin'] })
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className={montserrat.className}>
+    // <main className={montserrat.className}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </main>
+    // {/* </main> */}
     
   );
 }
